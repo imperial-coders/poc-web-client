69e774a78cbae7be102ac1d35cab925fd9447c36
Jason Hamm <jasonhamm.me@gmail.com>
2024-05-26 13:07:12 -0600

    Remove superfluous css class

--- components/table/index.tsx
+++ components/table/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const Table = ({ children }: { children: ReactNode }) => {
   return (
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       {children}
     </table>
   );
