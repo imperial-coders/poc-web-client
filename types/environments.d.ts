bfde33bf1c851b0419117112a7267ce73bf07dfe
Brett Adamson <adamsonb12@gmail.com>
2024-05-25 20:50:11 -0600

    udpated .env types

--- types/environments.d.ts
+++ types/environments.d.ts
@@ -1,8 +1,15 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      GRAPHQL_URL: string;
+      NEXT_PUBLIC_GRAPHQL_URL: string;
       TRANSACTIONS_URL: string;
+      NEXTAUTH_SECRET: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+      SERVICE_USERS_GRPC_HOST: string;
+      SERVICE_USERS_GRPC_SSL: string;
+      SERVICE_USERS_SETTINGS_GRPC_HOST: string;
+      SERVICE_USERS_SETTINGS_GRPC_SSL: string;
     }
   }
 }
