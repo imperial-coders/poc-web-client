6226a462f7ed73e93ecaf26508b0f8e23b8a6aad
Jason Hamm <jasonhamm.me@gmail.com>
2024-05-26 12:47:04 -0600

    Remove table wrapper

--- components/transactions/index.tsx
+++ components/transactions/index.tsx
@@ -14,33 +14,31 @@ export const Transactions = () => {
   ) : (
     <div className="flex flex-col gap-1">
       <div>{totalCount} - total rows</div>
-      <div>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeader />
-              <TableHeader>Id</TableHeader>
-              <TableHeader>Merchant</TableHeader>
-              <TableHeader>Summary</TableHeader>
-              <TableHeader>Amount</TableHeader>
-            </TableRow>
-          </TableHead>
-          <tbody>
-            {/* @ts-ignore */}
-            {transactions.map((t, index) => {
-              return (
-                <TableRow key={t.id}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{t.id}</TableCell>
-                  <TableCell>{t.merchant}</TableCell>
-                  <TableCell>{t.summary}</TableCell>
-                  <TableCell>{t.amountInCents}</TableCell>
-                </TableRow>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeader />
+            <TableHeader>Id</TableHeader>
+            <TableHeader>Merchant</TableHeader>
+            <TableHeader>Summary</TableHeader>
+            <TableHeader>Amount</TableHeader>
+          </TableRow>
+        </TableHead>
+        <tbody>
+          {/* @ts-ignore */}
+          {transactions.map((t, index) => {
+            return (
+              <TableRow key={t.id}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{t.id}</TableCell>
+                <TableCell>{t.merchant}</TableCell>
+                <TableCell>{t.summary}</TableCell>
+                <TableCell>{t.amountInCents}</TableCell>
+              </TableRow>
+            );
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 };
